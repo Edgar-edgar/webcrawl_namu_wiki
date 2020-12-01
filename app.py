1d7a067f585b11a51488948e4843dbcb414f4b23
Edgar <edgardelatorre99@gmail.com>
2020-12-01 19:49:42 +0800

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.mixer.init()
-pygame.mixer.music.load('kimi_no_toriko.mp3')
-pygame.mixer.music.play()
-
-while pygame.mixer.music.get_busy(): 
-    pygame.time.Clock().tick(10)
